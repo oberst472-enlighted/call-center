197c153c0ffa67c04f4ab18c9bbaecab0b521259
ilham <SergeyGuliy>
2020-07-03 15:26:19 +0300

    session from server

--- src/components/views/components/callInHistory.vue
+++ src/components/views/components/callInHistory.vue
@@ -3,7 +3,7 @@
     <hr>
     <div class="call" @click="$router.push(`/call-list/${data.id}`)">
       <div class="call-left">
-        <div class="call-left-date">{{dateCall}}  <span>{{startTime}} - {{endTime}}</span></div>
+        <div class="call-left-date">{{dateCall}}  <span>{{startTime}} - {{data.endTime ? endTime : ''}}</span></div>
         <div class="call-left-name">{{data.device.term.title}}</div>
         <div class="call-left-terminal">терминал #{{data.device.term.id}}</div>
         <div class="call-left-text">{{data.device.title}}</div>
@@ -54,85 +54,85 @@
 </script>
 
 <style lang='scss'>
-.call_in_history{
-  .call{
-    cursor: pointer;
-    margin: 12px 0;
-    display: flex;
-    justify-content: space-between;
-    color: #4c3b60;
-    font-size: 10px;
-    font-weight: 700;
-    line-height: 13px;
-    &-left{
-      &-date{
-        opacity: 0.7;
-        color: #65528b;
-        font-size: 11px;
-        font-weight: 500;
-        margin-bottom: 5px;
-        span{
-          font-weight: 500;
+  .call_in_history{
+    .call{
+      cursor: pointer;
+      margin: 12px 0;
+      display: flex;
+      justify-content: space-between;
+      color: #4c3b60;
+      font-size: 10px;
+      font-weight: 700;
+      line-height: 13px;
+      &-left{
+        &-date{
+          opacity: 0.7;
+          color: #65528b;
           font-size: 11px;
+          font-weight: 500;
+          margin-bottom: 5px;
+          span{
+            font-weight: 500;
+            font-size: 11px;
+          }
+        }
+        &-name{
+          color: #4a4355;
+          font-size: 12px;
+          font-weight: 400;
+        }
+        &-terminal{
+          font-size: 12px;
+          font-weight: 400;
+
+        }
+        &-text{
+          font-weight: 400;
         }
       }
-      &-name{
-        color: #4a4355;
-        font-size: 12px;
-        font-weight: 400;
-      }
-      &-terminal{
-        font-size: 12px;
-        font-weight: 400;
-
-      }
-      &-text{
-        font-weight: 400;
+      &-right{
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+        &-status{
+          text-align: center;
+          height: 14px;
+          border-radius: 7px;
+          background-color: #f2fcf4;
+          color: #4fd161;
+          font-size: 8px;
+          font-weight: 700;
+          width: 70px;
+        }
       }
     }
-    &-right{
+    .operator{
+      margin-top: 10px;
+      cursor: pointer;
       display: flex;
-      flex-direction: column;
-      justify-content: flex-start;
       align-items: center;
+      margin-bottom: 14px;
       &-status{
-        text-align: center;
+        width: 83px;
         height: 14px;
         border-radius: 7px;
         background-color: #f2fcf4;
         color: #4fd161;
         font-size: 8px;
-        font-weight: 700;
-        width: 70px;
+        text-align: center;
+      }
+      &-text{
+        color: #685c7b;
+        font-size: 10px;
+        font-weight: 400;
+        line-height: 12px;    }
+      &-img{
+        height: 40px;
+        width: 40px;
+        margin-right: 14px;
       }
     }
-  }
-  .operator{
-    margin-top: 10px;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    margin-bottom: 14px;
-    &-status{
-      width: 83px;
-      height: 14px;
-      border-radius: 7px;
-      background-color: #f2fcf4;
-      color: #4fd161;
-      font-size: 8px;
-      text-align: center;
-    }
-    &-text{
-      color: #685c7b;
-      font-size: 10px;
-      font-weight: 400;
-      line-height: 12px;    }
-    &-img{
-      height: 40px;
-      width: 40px;
-      margin-right: 14px;
-    }
-  }
 
-}
+  }
 </style>

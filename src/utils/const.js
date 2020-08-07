a8328023840bdc2942ba7bd753e3fde0da2ae769
Nikolai <kulaganikolai@yandex.ru>
2020-08-07 10:05:29 +0300

    Commit const.js fix

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,3 +1,3 @@
-export const baseAppUrl = 'https://calls-dev.enlighted.ru'
+export const baseAppUrl = process.env.VUE_APP_URL || 'https://calls-dev.enlighted.ru'
 
-export const appToken = '841604f050f5e9ec8fcab0489358215f571d4965'
+export const appToken =  process.env.VUE_APP_TOKEN || '841604f050f5e9ec8fcab0489358215f571d4965'

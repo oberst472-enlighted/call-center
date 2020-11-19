3249f7a400c07df6b614065a82100c541270109c
Александр Валесин <oberst47@gmail.com>
2020-11-19 18:09:03 +0300

    call=page

--- src/layouts/operator/default/index.vue
+++ src/layouts/operator/default/index.vue
@@ -19,19 +19,61 @@
                 </div>
             </section>
         </main>
+
+        <div class="layout-default__call-video" v-if="true">
+            <SectionCallVideo/>
+        </div>
     </div>
 </template>
 
 <script>
 import {customLog} from '@/utils/console-group'
 import {mapState, mapMutations, mapActions} from 'vuex'
+import SectionCallVideo from '@/components/sections/call-video'
+import RecordRTC from 'recordrtc'
+import axios from 'axios'
 
 export default {
+    components: {
+        SectionCallVideo
+    },
     data() {
         return {
             socket: null,
             socketRetryConnectTime: 5000, //повторно подключение при разрыве соединения,
-            audio: new Audio('/assets/call-melody.mp3')
+            audio: new Audio('/assets/call-melody.mp3'),
+
+            recoder: null,
+            isSocketOpen: false,
+
+            peer: null,
+
+            userStream: '',
+            partnerStream: '',
+
+            options: {audio: true, video: true},
+
+            callID: '',
+            clientChannel: '',
+            videoToken: '',
+            videoID: '',
+
+            constraints: {
+                iceServers: [
+                    {url: 'stun:stun1.l.google.com:19302'},
+                    {url: 'stun:stun2.l.google.com:19302'},
+                    {url: 'stun:stun3.l.google.com:19302'},
+                    {
+                        url: 'turn:coturn.sverstal.ru:3478',
+                        username: 'tab1',
+                        credential: '123456',
+                    },
+                ],
+            },
+            offerOptions: {
+                offerToReceiveAudio: true,
+                offerToReceiveVideo: true
+            },
         }
     },
     computed: {
@@ -133,6 +175,146 @@ export default {
                 }
             }
         },
+
+        _handleNewICECandidateMsg(incoming) {
+            const candidate = new RTCIceCandidate(incoming)
+            try {
+                this.peer.addIceCandidate(candidate)
+            } catch (e) {
+                this.log('_handleNewICECandidateMsg-1', candidate, 'red')
+                this.log('_handleNewICECandidateMsg-2', e, 'red')
+            }
+        },
+
+        async _createPeer() {
+            this.peer = await new RTCPeerConnection(this.constraints)
+
+            this.peer.onicecandidate = e => {
+                if (e.candidate) {
+
+                    const data = {
+                        to: this.clientChannel,
+                        message_data: {
+                            event: 'ice-candidate',
+                            data: {
+                                event: 'ice-candidate',
+                                candidate: e.candidate,
+                            }
+                        }
+                    }
+
+                    this.sendMessage('message_to', data)
+                }
+            }
+
+            this.peer.ontrack = e => {
+                if (e) {
+                    this.$refs.partnerVideo.srcObject = e.streams[0]
+                    this.partnerStream = e.streams[0]
+                    this.log('ontrack', 'Монтирование видео партнера', 'lightgreen')
+                } else {
+                    this.log('ontrack', e, 'red')
+                }
+            }
+            // eslint-disable-next-line require-await
+
+            const stream = await navigator.mediaDevices.getUserMedia(this.options)
+
+            this.$refs.userVideo.srcObject = stream
+            this.userStream = stream
+            console.log(this.userStream)
+        },
+
+        async _createAnswer(payload) {
+            await this._createPeer()
+            const desc = await new RTCSessionDescription(payload.sdp)
+            this.userStream.getTracks().forEach(track => this.peer.addTrack(track, this.userStream))
+
+            //передаем offer терминала в в webRTC с помощью setRemoteDescription
+            await this.peer.setRemoteDescription(desc)
+
+            const answer = await this.peer.createAnswer(this.offerOptions)
+            await this.peer.setLocalDescription(answer)
+            const data = {
+                to: this.clientChannel,
+                message_data: {
+                    event: 'answer',
+                    data: {
+                        sdp: this.peer.localDescription
+                    }
+                }
+            }
+            this.sendMessage('message_to', data)
+        },
+
+
+        pickUpThePhone() {
+            const data = {
+                call_id: this.callID
+            }
+            this.sendMessage('picked_up', data)
+        },
+        sendMessage(eventName, data) {
+            const payload = {
+                event: eventName,
+                data
+            }
+            this.socket.send(this.getStringFromJson(payload))
+        },
+
+        stopCall() {
+            this.stopRecord()
+        },
+
+        startRecord() {
+            this.recorder = RecordRTC([this.userStream, this.partnerStream], {
+                type: 'video',
+                checkForInactiveTracks: true,
+                timeSlice: 1000,
+                // ondataavailable(blob) {
+                //     console.log('has data');
+                // },
+                // onStateChange(state) {
+                //     console.log(state)
+                // },
+            })
+            this.recorder.startRecording()
+        },
+        stopRecord() {
+
+            if (this.recorder) {
+                this.recorder.stopRecording(() => {
+                    const blob = this.recorder.getBlob()
+                    console.log(blob)
+                    const data = new FormData()
+
+                    data.append('video_file', blob, 'long.webm')
+
+                    console.log(data)
+                    console.log(`Authorization: token ${this.videoToken}`)
+                    const lol = `/videos/${this.videoID}/`
+                    axios.patch(lol, data, {
+                        headers: {
+                            'Content-Type': 'multipart/form-data',
+                            Authorization: `token ${this.videoToken}`
+                        },
+                    }).then(result => {
+                        console.log(result)
+                    })
+                        .catch(e => {
+                            console.log(e)
+                        })
+
+                    this.recorder.destroy()
+                    this.recorder = null
+                })
+            } else {
+                console.log('recorder not found')
+            }
+
+
+        },
+
         startAudio() {
             const promise = this.audio.play()
             if (promise !== undefined) {
@@ -152,6 +334,11 @@ export default {
                     if (val) {
                         this.startAudio()
                     }
+                }
+            },
+            partnerStream() {
+                if (this.userStream) {
+                    this.startRecord()
                 }
             }
         },
@@ -196,8 +383,14 @@ export default {
             display: flex;
             width: inherit;
         }
+    }
 
-
+    &__call-video {
+        position: fixed;
+        top: 0;
+        left: 0;
+        background-color: red;
+        z-index: 10;
     }
 
     &__main {

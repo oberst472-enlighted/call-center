10cdcd2b98a4a137dadfe564b140cab6c4d50202
Artem Ulko <ulkoart@gmail.com>
2020-11-19 15:00:08 +0300

    upd CI

---
+++ etc/tg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TOKEN="782104901:AAEaWwN4M7PxT3s2O9W3DmcAAMxwtVp_3rA"
+CHAT_ID="-263138756"
+MESSAGE=$1
+URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+
+curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$MESSAGE"

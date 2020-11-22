738c296ba3a9b6354b91ed0c3be73602aadebc6a
Александр Валесин <oberst47@gmail.com>
2020-11-22 13:47:43 +0300

    urls

--- src/store/modules/socket/index.js
+++ src/store/modules/socket/index.js
@@ -18,11 +18,13 @@ export default {
                 //     },
                 // ],
                 iceServers: [
-                    {url: 'stun:stun1.l.google.com:19302'},
-                    {url: 'stun:stun2.l.google.com:19302'},
-                    {url: 'stun:stun3.l.google.com:19302'},
+                    {urls: 'stun:stun.l.google.com:19302'},
+                    {urls: 'stun:stun1.l.google.com:19302'},
+                    {urls: 'stun:stun2.l.google.com:19302'},
+                    {urls: 'stun:stun3.l.google.com:19302'},
+                    {urls: 'stun:stun4.l.google.com:19302'},
                     {
-                        url: 'turn:coturn.sverstal.ru:3478',
+                        urls: 'turn:coturn.sverstal.ru:3478',
                         username: 'tab1',
                         credential: '123456',
                     },

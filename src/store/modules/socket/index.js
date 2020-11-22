ccbe478da939899f463dfb29722b57db9f1b56ce
Александр Валесин <oberst47@gmail.com>
2020-11-22 13:00:56 +0300

    stun urls add port

--- src/store/modules/socket/index.js
+++ src/store/modules/socket/index.js
@@ -7,10 +7,10 @@ export default {
         options: {
             constraints: {
                 iceServers: [
-                    { urls: 'stun:stun.l.google.com' },
-                    { urls: 'stun:stun1.l.google.com' },
-                    { urls: 'stun:stun2.l.google.com' },
-                    { urls: 'stun:stun3.l.google.com' },
+                    { urls: 'stun:stun.l.google.com:19302' },
+                    { urls: 'stun:stun1.l.google.com:19302' },
+                    { urls: 'stun:stun2.l.google.com:19302' },
+                    { urls: 'stun:stun3.l.google.com:19302' },
                     {
                         urls: 'turn:vc-dev.enlighted.ru:3478',
                         username: 'tab1',

3c069f554aebde02819ef82008de37ade610d904
Александр Валесин <oberst47@gmail.com>
2020-11-23 23:25:43 +0300

    iseConfig

--- src/store/modules/socket/index.js
+++ src/store/modules/socket/index.js
@@ -6,17 +6,6 @@ export default {
     state: {
         options: {
             constraints: {
-                // iceServers: [
-                //     { urls: 'stun:stun.l.google.com:19302' },
-                //     { urls: 'stun:stun1.l.google.com:19302' },
-                //     { urls: 'stun:stun2.l.google.com:19302' },
-                //     { urls: 'stun:stun3.l.google.com:19302' },
-                //     {
-                //         urls: 'turn:vc-dev.enlighted.ru:3478',
-                //         username: 'tab1',
-                //         credential: '123456',
-                //     },
-                // ],
                 iceServers: [
                     { url: 'stun:stun1.l.google.com:19302' },
                     { url: 'stun:stun2.l.google.com:19302' },
